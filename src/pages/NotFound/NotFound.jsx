717d70ed36a41b2d34a633a6720eb41010b4cb2a
Sasha.Solo <sasha.solomannik@gmail.com>
2023-01-08 02:15:28 +0200

    add pages

---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,5 @@
+import { Text } from './NotFound.styled';
+
+export const NotFound = () => {
+  return <Text>There is no information. Please, come back on Home page</Text>;
+};
---
+++ src/pages/NotFound/NotFound.styled.jsx
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Text = styled.div`
+  text-transform: uppercase;
+  color: #f74002;
+`;
