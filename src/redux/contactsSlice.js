574427b31eabd0f31b87bb825bd4f6b4f8d4abf3
Sasha.Solo <sasha.solomannik@gmail.com>
2023-01-14 00:30:08 +0200

    commit redux

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,24 +1,48 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { fetchContacts, addContact, deleteContact } from './operations';
+
+const handlePending = state => {
+  state.isLoading = true;
+};
+
+const handleRejected = (state, action) => {
+  state.isLoading = false;
+  state.error = action.payload;
+};
 
 export const contactsSlice = createSlice({
   name: 'contacts',
-  initialState: [],
-  reducers: {
-    addContact: {
-      reducer(state, action) {
-        state.push(action.payload);
-      },
+  initialState: {
+    contacts: [],
+    isLoading: false,
+    error: null,
+  },
+  extraReducers: {
+    [fetchContacts.pending]: handlePending,
+    [fetchContacts.fulfilled](state, action) {
+      state.isLoading = false;
+      state.error = null;
+      state.contacts = action.payload;
     },
-    deleteContact(state, action) {
-      state.splice(
-        0,
-        state.length,
-        ...state.filter(contact => contact.id !== action.payload)
+    [fetchContacts.rejected]: handleRejected,
+    [addContact.pending]: handlePending,
+    [addContact.fulfilled](state, action) {
+      state.isLoading = false;
+      state.error = null;
+      state.contacts.push(action.payload);
+    },
+    [addContact.rejected]: handleRejected,
+    [deleteContact.pending]: handlePending,
+    [deleteContact.fulfilled](state, action) {
+      state.isLoading = false;
+      state.error = null;
+      const index = state.contacts.findIndex(
+        contact => contact.id === action.payload.id
       );
+      state.contacts.splice(index, 1);
     },
+    [deleteContact.rejected]: handleRejected,
   },
 });
-
-export const { addContact, deleteContact } = contactsSlice.actions;
 
 export const contactsReducer = contactsSlice.reducer;
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,3 +1,7 @@
 export const getContacts = state => state.contacts;
 
 export const getInputFilter = state => state.filter.text;
+
+export const getLoading = state => state.contacts.isLoading;
+
+export const getError = state => state.contacts.error;
