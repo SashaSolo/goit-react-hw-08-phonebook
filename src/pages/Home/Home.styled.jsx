bedbf0160ad0238f552baa08287abdcfaea89d73
Sasha.Solo <sasha.solomannik@gmail.com>
2023-02-03 17:31:05 +0200

    add home

---
+++ src/pages/Home/Home.styled.jsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  margin-top: 20px;
+`;
+
+export const Title = styled.h1`
+  font-size: 62px;
+  color: #003030;
+`;
+
+export const Image = styled.img`
+  width: 40%;
+  border-radius: 20px;
+`;
