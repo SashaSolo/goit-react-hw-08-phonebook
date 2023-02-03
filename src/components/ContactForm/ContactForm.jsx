a43e22cbb342bbab362786f44300d34e49ca223f
Sasha.Solo <sasha.solomannik@gmail.com>
2023-02-03 17:31:56 +0200

    fix contact form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,13 +1,14 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { Btn } from 'components/Button/Button';
-import { getContacts } from 'redux/auth/selectors';
+// import { Button } from 'components/Button/Button';
+import { selectContacts } from 'redux/contacts/selectors';
 import { addContact } from 'redux/contacts/contOperations';
+import PropTypes from 'prop-types';
 
-import { FormStyle, Label, Input } from './ContactForm.styled';
+import { Form, Label, Input, Wrapper, Title, Btn } from './ContactForm.styled';
 
 export function ContactForm() {
   const dispatch = useDispatch();
-  const contacts = useSelector(getContacts);
+  const contacts = useSelector(selectContacts);
 
   const handleSubmit = event => {
     event.preventDefault();
@@ -15,12 +16,13 @@ export function ContactForm() {
     const form = event.currentTarget;
     const name = form.elements['name'].value;
 
-    const phone = String(form.elements['phone'].value);
+    const number = String(form.elements['number'].value);
 
     const normalizedName = name.toLowerCase();
 
-    const compareNames = contacts.find(
-      contactToCompare => contactToCompare.name.toLowerCase() === normalizedName
+    const compareNames = contacts?.find(
+      contactToCompare =>
+        contactToCompare.name?.toLowerCase() === normalizedName
     );
 
     if (compareNames) {
@@ -28,34 +30,47 @@ export function ContactForm() {
       return;
     }
 
-    dispatch(addContact({ name, phone }));
+    dispatch(addContact({ name, number }));
     form.reset();
   };
 
   return (
-    <div>
-      <FormStyle onSubmit={handleSubmit}>
-        <Label htmlFor="1">Name</Label>
+    <Wrapper>
+      <div>
+        <Title>Add Contacts</Title>
+        <Form onSubmit={handleSubmit}>
+          <Label htmlFor="1">
+            {' '}
+            Name{' '}
+            <Input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+              id="1"
+            />
+          </Label>
 
-        <Input
-          type="text"
-          name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-          id="1"
-        />
-        <Label htmlFor="2">Phone</Label>
-        <Input
-          type="tel"
-          name="phone"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          required
-          id="2"
-        />
-        <Btn type="submit">Add contact</Btn>
-      </FormStyle>
-    </div>
+          <Label htmlFor="2">
+            Phone{' '}
+            <Input
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+              id="2"
+            />
+          </Label>
+
+          <Btn type="submit">Add contact</Btn>
+        </Form>
+      </div>
+    </Wrapper>
   );
 }
+
+ContactForm.propTypes = {
+  handleSubmit: PropTypes.func,
+};
