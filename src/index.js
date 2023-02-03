4221406f60e8f5fe767e063d7928229f8c6c887c
Sasha.Solo <sasha.solomannik@gmail.com>
2023-02-03 17:29:35 +0200

    fix index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
-import { store, persistor } from 'redux/store';
 import { Provider } from 'react-redux';
+import { store, persistor } from 'redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import { App } from 'components/App/App';
+import { GlobalStyle } from './components/GlobalStyle';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -14,6 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <App />
+          <GlobalStyle />
         </PersistGate>
       </Provider>
     </BrowserRouter>
